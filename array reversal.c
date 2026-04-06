b733d66ba728aae5235272fec1b11b69974b4bda
mareeswarirajanbabu2002mca27-coder <mareeswarirajanbabu2002_mca27@mepcoeng.ac.in>
2026-04-05 23:20:55 -0700

    Create array reversal.c

---
+++ array reversal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    /* Write the logic to reverse the array. */
+    for(i = 0; i < num / 2; i++) {
+        // Swap elements at index i and index (num - 1 - i)
+        int temp = arr[i];
+        arr[i] = arr[num - 1 - i];
+        arr[num - 1 - i] = temp;
+    }
+
+    // Output the reversed array
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+        
+    free(arr);
+    return 0;
+}
